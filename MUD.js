003384fd99716162009f86ffca8f371ae1d9f6d9
tactnician <tactnician@gmail.com>
2019-10-06 22:18:20 -0400

    Initial Master Classes

---
+++ MUD.js
@@ -0,0 +1,107 @@
+
+/// Battle Palace MUD
+
+const wepArr = [] /// on creation weapon added to Array
+
+
+// RoomMC 
+ class Room {
+ 	constructor(name = '', desc = '', adjRms = {} , wepInRoom = []){
+ 		this.name = name
+ 		this.desc = desc 
+ 		this.adjRms = adjRms
+ 		this. wepInRoom = wepInRoom
+ 	}
+ }
+
+Room.wepInRoom = wepArr[
+	Math.floor(Math.random() * Math.floor(array.length -1))
+]
+
+// WeaponsMC 
+class Weapon {
+	constructor(name, desc, type, maxDamage){
+		this.name = name
+		this.desc = desc
+		this.type = type
+		this.maxDamage = maxDamage
+	}
+}
+
+Weapon.prototype.addToWepArr = function(){}
+
+
+
+// PersonaMC
+class Persona {
+	constructor(name, desc, health, atkSpeed, strength, skill){
+		this.name = name
+		this.desc = desc 
+		this.health = health
+		this.atkSpeed = atkSpeed
+		this.strength = strength
+		this.skill = skill
+	}
+}
+
+
+class Hero extends Persona {
+	constructor(name, desc, health, atkSpeed, strength, skill){
+		super(name, desc, health, atkSpeed, strength, skill)
+	}
+}
+
+/// Portal "where the game begins"
+const Foyer = new Room;
+
+Foyer.name = 'Foyer'
+
+Foyer.desc = ` 
+	You enter the foyer through a large wooden door. 
+	To the north is a dark room where you will face an unknown enemy in battle. 
+	It is the quickest route to victory but also the most difficult
+	`
+Foyer.adjRms = {
+	North: Conservatory,
+	East: SEHallway, 
+	West: SWHallway,
+	South: ExitGame 
+}
+
+
+
+
+// Names of Rooms in Mansion
+/*
+Kitchen 
+Ballroom 
+Library 
+TheHall
+Hall 
+Lounge 
+Cellar
+Billard room 
+Conservatory 
+Parlor 
+*/
+
+
+
+
+
+/// 
+
+
+/*
+Steps 
+1. a map comprised of room classes
+2. methods to move between rooms / user prompted.  
+3. weapons that can move between rooms 
+3.1 can only have one weapon at a time
+3.2 weapons change user states 
+4. methods to pickUp, putDown and carry objects
+5. game personas that can battle one another
+6. methods for having personas do battle 
+6.1 perhaps some Math.random() to determine who wins
+
+*/
