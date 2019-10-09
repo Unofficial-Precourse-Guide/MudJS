c707915a436f12c3b3a3bf98302926d8a52d442b
SebastianGarces <gsgarces1@gmail.com>
2019-10-08 23:42:26 -0400

    merged .js files to avoid import/esport/require() issues --- added readline as a dev dependency for terminal input

--- MUD.js
+++ MUD.js
@@ -1,7 +1,30 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+/*
+Steps 
+1. a map comprised of room classes
+2. methods to move between rooms / user prompted.  
+3. weapons that can move between rooms 
+3.1 can only have one weapon at a time
+3.2 weapons change user states 
+4. methods to pickUp, putDown and carry objects
+5. game personas that can battle one another
+6. methods for having personas do battle 
+6.1 perhaps some Math.random() to determine who wins
+
+*/
+
+
 /// Battle Palace MUD
 
+
+
 // RoomMC 
-export class Room {
+class Room {
 	constructor(name = '', desc = '', adjRms = {}, wepInRoom = []) {
 		this.name = name
 		this.desc = desc
@@ -10,98 +33,16 @@ export class Room {
 	}
 }
 
-// WeaponsMC 
-// 0 > maxDamage > 100
-export class Weapon {
-	constructor(name = '', desc = '', type, maxDamage = 0) {
-		this.name = name
-		this.desc = desc
-		this.type = type
-		this.maxDamage = maxDamage
-	}
-}
-
-// ['Sword', 'Axe', 'Gun', 'Bat', 'Throwing Star', 'Hammer', 'Flame Thrower', 'Candlestick', 'Dagger', 'Machete', 'ChainSaw']
-
-export const Sword = new Weapon;
-Sword.name = 'Sword';
-Sword.desc = 'Double edge sword';
-Sword.type = 'Fire';
-Sword.maxDamage = 50;
-
-
-
-export const Axe = new Weapon;
-Axe.name = 'Axe';
-Axe.desc = `1700's Battle Axe`;
-Axe.type = 'Metal';
-Axe.maxDamage = 65;
-
-
-
-// PersonaMC
-// 0 > health > 100
-// 0 > atkSpeed > 100
-// 0 > strength > 100
-// 0 > skill > 100
-export class Persona {
-	constructor(name = '', desc = '', health = 0, atkSpeed = 0, strength = 0, skill = 0) {
-		this.name = name
-		this.desc = desc
-		this.health = health
-		this.atkSpeed = atkSpeed
-		this.strength = strength
-		this.skill = skill
-	}
-}
-
-export const Godzilla = new Persona;
-Godzilla.name = `Godzilla`;
-Godzilla.desc = `
-				Godzilla is depicted as an enormous, destructive, prehistoric sea monster awakened and empowered 
-				by nuclear radiation
-				`;
-Godzilla.health = 240;
-Godzilla.atkSpeed = 3;
-Godzilla.strength = 55;
-Godzilla.skill = 50;
-
-
-
-export class Hero extends Persona {
-	constructor(name, desc, health, atkSpeed, strength, skill) {
-		super(name, desc, health, atkSpeed, strength, skill)
-	}
-}
-
-/// Portal "where the game begins"
-export const Foyer = new Room;
-
+const Foyer = new Room;
 Foyer.name = 'Foyer'
-
-Foyer.desc = ` 
-	You enter the Foyer through a large wooden door. 
-	To the north is a dark room where you will face an unknown enemy in battle. 
-	It is the quickest route to victory but also the most difficult
-	`
+Foyer.desc = `You enter the Foyer through a large wooden door. To the north is a dark room where you will face an unknown enemy in battle. It is the quickest route to victory but also the most difficult`;
+// Foyer.wepInRoom = wepArr
 // Foyer.adjRms = {
 // 	North: Conservatory,
 // 	East: SEHallway,
 // 	West: SWHallway,
 // 	South: ExitGame
 // }
-
-export const wepArr = [Sword, Axe]; /// on creation weapon added to Array
-export const roomArr = [Foyer];
-export const personasArr = [Hero, Godzilla];
-
-
-
-
-
-
-
-
 
 // Names of Rooms in Mansion
 /*
@@ -118,22 +59,136 @@ Parlor
 */
 
 
+// WeaponsMC 
+// 0 > maxDamage > 100
+class Weapon {
+	constructor(name = '', desc = '', type, maxDamage = 0) {
+		this.name = name
+		this.desc = desc
+		this.type = type
+		this.maxDamage = maxDamage
+	}
+}
+
+// ['Sword', 'Axe', 'Gun', 'Bat', 'Throwing Star', 'Hammer', 'Flame Thrower', 'Candlestick', 'Dagger', 'Machete', 'ChainSaw']
+
+const Sword = new Weapon;
+Sword.name = 'Sword';
+Sword.desc = 'Double edge sword';
+Sword.type = 'Fire';
+Sword.maxDamage = 50;
+
+const Axe = new Weapon;
+Axe.name = 'Axe';
+Axe.desc = `1700's Battle Axe`;
+Axe.type = 'Metal';
+Axe.maxDamage = 65;
+
+const Gun = new Weapon;
+Gun.name = 'Gun';
+Gun.desc = `Double Barrel`;
+Gun.type = 'Metal';
+Gun.maxDamage = 75;
+
+const wepArr = [Sword, Axe, Gun]; /// on creation weapon added to Array
 
 
 
-/// 
+// PersonaMC
+// 0 > health > 100
+// 0 > atkSpeed > 100
+// 0 > strength > 100
+// 0 > skill > 100
+class Persona {
+	constructor(name = '', desc = '', health = 0, atkSpeed = 0, strength = 0, skill = 0) {
+		this.name = name
+		this.desc = desc
+		this.health = health
+		this.atkSpeed = atkSpeed
+		this.strength = strength
+		this.skill = skill
+	}
+}
+
+const Godzilla = new Persona;
+Godzilla.name = `Godzilla`;
+Godzilla.desc = `Godzilla is depicted as an enormous, destructive, prehistoric sea monster awakened and empowered by nuclear radiation`;
+Godzilla.health = 240;
+Godzilla.atkSpeed = 3;
+Godzilla.strength = 55;
+Godzilla.skill = 50;
+
+
+
+class Hero extends Persona {
+	constructor(name, desc, health, atkSpeed, strength, skill) {
+		super(name, desc, health, atkSpeed, strength, skill)
+	}
+}
+
+
+/// Portal "where the game begins"
+
+const personasArr = [Hero, Godzilla];
+const roomArr = [Foyer];
+
+
 
 
 /*
-Steps 
-1. a map comprised of room classes
-2. methods to move between rooms / user prompted.  
-3. weapons that can move between rooms 
-3.1 can only have one weapon at a time
-3.2 weapons change user states 
-4. methods to pickUp, putDown and carry objects
-5. game personas that can battle one another
-6. methods for having personas do battle 
-6.1 perhaps some Math.random() to determine who wins
-
+:'######::::::'###::::'##::::'##:'########::::'##::::::::'#######:::'######:::'####::'######::
+'##... ##::::'## ##::: ###::'###: ##.....::::: ##:::::::'##.... ##:'##... ##::. ##::'##... ##:
+ ##:::..::::'##:. ##:: ####'####: ##:::::::::: ##::::::: ##:::: ##: ##:::..:::: ##:: ##:::..::
+ ##::'####:'##:::. ##: ## ### ##: ######:::::: ##::::::: ##:::: ##: ##::'####:: ##:: ##:::::::
+ ##::: ##:: #########: ##. #: ##: ##...::::::: ##::::::: ##:::: ##: ##::: ##::: ##:: ##:::::::
+ ##::: ##:: ##.... ##: ##:.:: ##: ##:::::::::: ##::::::: ##:::: ##: ##::: ##::: ##:: ##::: ##:
+. ######::: ##:::: ##: ##:::: ##: ########:::: ########:. #######::. ######:::'####:. ######::
+:......::::..:::::..::..:::::..::........:::::........:::.......::::......::::....:::......:::
 */
+
+
+const setWpnToRdn = function (wepQty) { // Select random weapon by index of 0 - weapons.length
+	return wepArr[Math.trunc(Math.random() * wepQty)];
+}
+
+
+
+const game = () => {
+	let currentRoom = Foyer;
+	let exit = false;
+	// let wepInRoom = [];
+	while (!exit) {
+		console.log(`
+
+
+:'######::'########::::'###::::'########::'########:::::'######::::::'###::::'##::::'##:'########:
+'##... ##:... ##..::::'## ##::: ##.... ##:... ##..:::::'##... ##::::'## ##::: ###::'###: ##.....::
+ ##:::..::::: ##:::::'##:. ##:: ##:::: ##:::: ##::::::: ##:::..::::'##:. ##:: ####'####: ##:::::::
+. ######::::: ##::::'##:::. ##: ########::::: ##::::::: ##::'####:'##:::. ##: ## ### ##: ######:::
+:..... ##:::: ##:::: #########: ##.. ##:::::: ##::::::: ##::: ##:: #########: ##. #: ##: ##...::::
+'##::: ##:::: ##:::: ##.... ##: ##::. ##::::: ##::::::: ##::: ##:: ##.... ##: ##:.:: ##: ##:::::::
+. ######::::: ##:::: ##:::: ##: ##:::. ##:::: ##:::::::. ######::: ##:::: ##: ##:::: ##: ########:
+:......::::::..:::::..:::::..::..:::::..:::::..:::::::::......::::..:::::..::..:::::..::........::
+
+
+		`)
+		console.log(currentRoom.desc);
+		switch (currentRoom) {
+			case Foyer:
+				currentRoom.wepInRoom.push(setWpnToRdn(wepArr.length));
+				currentRoom.wepInRoom.push(setWpnToRdn(wepArr.length));
+				currentRoom.wepInRoom.push(setWpnToRdn(wepArr.length));
+				console.log(`This room has this weapons: ${currentRoom.wepInRoom[0].name.toString()}, ${currentRoom.wepInRoom[1].name.toString()}, ${currentRoom.wepInRoom[2].name.toString()}`)
+				console.table(currentRoom.wepInRoom);
+				rl.question('select your weapon...  ', (answer) => {
+					console.log(`you have selected ${answer}`);
+					rl.close();
+				})
+				exit = true;
+				break;
+
+		}
+	}
+}
+
+game();
